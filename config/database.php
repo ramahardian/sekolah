af88e19ee72d148752fb6247cbaa81a155a9ae1e
ramahardian <ramahardianstudio@gmail.com>
2026-03-15 02:34:04 +0700

    l

--- config/database.php
+++ config/database.php
@@ -10,6 +10,7 @@ $user = "u778324865_sekolah";
 $pass = "7Iu|zc&k8";
 $db = "u778324865_sekolah";
 
+
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
